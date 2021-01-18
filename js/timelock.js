e4313ad13648c0ec9fb3ad849aff777cf4a307f6
mitch <mitchell.negus.57@gmail.com>
2021-01-18 14:00:44 -0500

    locking the screen until presentation goes live

---
+++ js/timelock.js
@@ -0,0 +1,22 @@
+/*
+ * Lock the presentation so that it may not be viewed online until the presentation goes live.
+ */
+
+(function () {
+
+	const end = new Date(2021, 1, 1, 11, 30, 0);
+	const now = new Date();
+
+	// If the end of the lock time period is still in the future, hide the content
+	if (end - now > 0) {
+		// Prepare the lockscreen text
+		const lockScreen = document.getElementById('lock-screen');
+		const releaseDate = document.getElementById('release-date');
+		// Show the lock screen
+		lockScreen.style.display = 'block';
+		releaseDate.innerHTML = end.toLocaleString();
+		// Replace the locked region by the lockscreen text
+		const lockedRegion = document.getElementById('locked');
+		lockedRegion.remove();
+	}
+})();
